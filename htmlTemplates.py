084ad6551e285af491c569148d48c649bd25c0c9
parth_1egend <parthkansagra123@gmail.com>
2024-05-15 23:00:58 +0530

    Inital Phase Basic

---
+++ htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://static.vecteezy.com/system/resources/previews/021/059/825/non_2x/chatgpt-logo-chat-gpt-icon-on-green-background-free-vector.jpg" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://media.licdn.com/dms/image/D4D03AQExeICF7FIIgg/profile-displayphoto-shrink_800_800/0/1691351783229?e=2147483647&v=beta&t=LcUUST-LoxfKKwItUENSEfPon249nn3L3h_AYiRxrUo">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
