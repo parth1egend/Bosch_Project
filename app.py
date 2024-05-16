239b37647acebc832e0a36507ac0cba4d7c511c5
parth_1egend <parthkansagra123@gmail.com>
2024-05-16 16:17:06 +0530

    Fix an error

--- app.py
+++ app.py
@@ -21,9 +21,9 @@ def get_pdf_text(pdf_docs):
 
 def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
-        separator="\n",
+        separator=" ",
         chunk_size=1000,
-        chunk_overlap=200,
+        chunk_overlap=100,
         length_function=len
     )
     chunks = text_splitter.split_text(text)
@@ -31,16 +31,18 @@ def get_text_chunks(text):
 
 
 def get_vectorstore(text_chunks):
-    
+    #embeddings = OpenAIEmbeddings()
     embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
 
 def get_conversation_chain(vectorstore):
-    
+    # llm = ChatOpenAI()
     llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-
+    
+    llm.client.api_url = 'https://api-inference.huggingface.co/models/google/flan-t5-xxl'
+    
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
@@ -62,8 +64,6 @@ def handle_userinput(user_question):
         else:
             st.write(bot_template.replace(
                 "{{MSG}}", message.content), unsafe_allow_html=True)
-            
-    
 
 
 def main():
